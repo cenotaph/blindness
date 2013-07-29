5615926265f0300af5703ee635a64474f9df32fc
John W. Fail <fail@cenotaph.org>
2013-07-30 02:28:22 +0300

    force hostname into asset_host for RSS images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Blindness::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_controller.asset_host =  "http://blindness.icewhistle.com"
+ 
 end
