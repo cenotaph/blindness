60c56b380e457db31ce4aa777792888283aa6ee3
John W. Fail <fail@cenotaph.org>
2013-07-30 16:53:48 +0300

    basic CMS for actually making posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < InheritedResources::Base
   has_scope :page, :default => 1
   respond_to :html, :rss
+  before_filter :authenticate_user!, :only => [:create, :update, :new, :edit, :destroy]
   
+  def edit
+    @post = Post.friendly.find(params[:id])
+    render :template => 'posts/forms/' + @post.mytype
+  end
   def filtered
     @posts = Post.where(['lower(title) = ?', params[:id]]).page(params[:page]).per(10)
     @filtered = params[:id]
@@ -9,6 +14,14 @@ class PostsController < InheritedResources::Base
   end
   
 
+  def new
+    @post = Post.new
+    if params[:type]
+        render :template => 'posts/forms/' + params[:type]
+    else
+        render :template => 'posts/forms/post'
+    end
+  end
   
   def search
     if params[:searchterm].strip.size < 4
@@ -29,6 +42,19 @@ class PostsController < InheritedResources::Base
     else
       @post = Post.friendly.find(params[:id])
     end
+  end
+  
+  def update
+    @post = Post.friendly.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :user_id, :localimage, :localfile, :url, :description, :the_type)) 
+      flash[:notice] = 'Post updated'
+      redirect_to @post
+    end
+  end
+  protected
+  
+  def permitted_params
+    params.permit(:post => [:title, :user_id, :localimage, :localfile, :url, :description, :the_type])
   end
   
 end
