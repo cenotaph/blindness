825c83737bc729cc4169035fe2b84fbf71011b4f
John W. Fail <john@pixelache.ac>
2016-12-01 11:46:07 +0200

    secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blindness::Application.config.secret_key_base = '2868828dde2a6cf3bba9d61538f6335e3f6e4c26acd6e933d2bab78cbed949cfaf2b0cd8d00ea32d0fedb1364500c10d3fd3d0c37886cadfbcd355099222c239'
+Blindness::Application.config.secret_key_base = ENV['secret_token']
+
